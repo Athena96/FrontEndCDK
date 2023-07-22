5bc9579c9e6c6fc3e83993431d646237a2a4d872
Jared Franzone <jffranzo@amazon.com>
2023-07-22 12:32:12 -0700

    Initial commit

---
+++ test/front_end_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FrontEndCdk from '../lib/front_end_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/front_end_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FrontEndCdk.FrontEndCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
